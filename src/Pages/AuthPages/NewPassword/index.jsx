8f7d13792a2d9f5100d047914e2100f1ad3e254d
RakhmatullohR <rrr.engineer.94@gmail.com>
2023-04-26 07:43:38 +0500

    first commit

--- src/Pages/AuthPages/NewPassword/index.jsx
+++ src/Pages/AuthPages/NewPassword/index.jsx
@@ -1,84 +1,128 @@
 import { LockOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../../context/Auth';
 import { FormWrapper, Wrapper } from './style';
 const NewPasswordPage = () => {
+  const [, dispatch] = useAuth();
+  const [success, setSuccess] = useState(false);
   const onFinish = (values) => {
-    console.log('Received values of form: ', values);
+    const formData = {
+      email: values.email || null,
+      email: values.email || null,
+    };
+    fetch(
+      'https://education07.pythonanywhere.com/auth/users/reset_password_confirm/',
+      {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw response.json();
+        } else {
+          return response.json();
+        }
+      })
+      .then(async (res) => {
+        console.log('res', res);
+        setSuccess(true);
+      })
+      .catch((Err) => {
+        Err.then((err) => err).then((err) => {
+          console.log('err', err);
+          message.warning(err?.detail);
+        });
+      });
   };
   return (
     <Wrapper>
       <FormWrapper>
-        <Form
-          layout='vertical'
-          name='normal_login'
-          className='login-form'
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-        >
-          <div style={{ margin: '-15px 0 5px' }}>
-            <h3>Have you forgotten your password?</h3>
-          </div>
-          <Form.Item
-            name='password'
-            label='New Password'
-            rules={[
-              {
-                required: true,
-                message: 'Please input your new password!',
-              },
-            ]}
-            hasFeedback
+        {success ? (
+          <Form
+            layout='vertical'
+            name='normal_login'
+            className='login-form'
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
           >
-            <Input.Password
-              prefix={<LockOutlined className='site-form-item-icon' />}
-              type='password'
-              placeholder='New Password'
-            />
-          </Form.Item>
-
-          <Form.Item
-            name='confirm'
-            label='Confirm New Password'
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your new password!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error(
-                      'The two passwords that you entered do not match!'
-                    )
-                  );
+            <div style={{ margin: '-15px 0 5px' }}>
+              <h3>Have you forgotten your password?</h3>
+            </div>
+            <Form.Item
+              name='password'
+              label='New Password'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your new password!',
                 },
-              }),
-            ]}
-          >
-            <Input.Password
-              prefix={<LockOutlined className='site-form-item-icon' />}
-              type='password'
-              placeholder='Confirm New Password'
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type='primary'
-              htmlType='submit'
-              className='login-form-button'
+              ]}
+              hasFeedback
             >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+              <Input.Password
+                prefix={<LockOutlined className='site-form-item-icon' />}
+                type='password'
+                placeholder='New Password'
+              />
+            </Form.Item>
+
+            <Form.Item
+              name='confirm'
+              label='Confirm New Password'
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your new password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        'The two passwords that you entered do not match!'
+                      )
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className='site-form-item-icon' />}
+                type='password'
+                placeholder='Confirm New Password'
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='login-form-button'
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        ) : (
+          <div style={{ textAlign: 'center' }}>
+            <h2 style={{ color: 'green' }}>✔ Signed In Successfully</h2>
+            <Link to='/main'>
+              Go to <b>Signin</b> page
+            </Link>
+          </div>
+        )}
       </FormWrapper>
     </Wrapper>
   );
--- src/Pages/AuthPages/ResetPassword/index.jsx
+++ src/Pages/AuthPages/ResetPassword/index.jsx
@@ -1,48 +1,84 @@
-import { MailOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { Button, Form, Input, message } from 'antd';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../../context/Auth';
+import { getCookie, setCookie } from '../../../utils/formatters';
 import { FormWrapper, Wrapper } from './style';
 const ResetPasswordPage = () => {
+  const [, dispatch] = useAuth();
+  const [success, setSuccess] = useState(false);
   const onFinish = (values) => {
-    console.log('Received values of form: ', values);
+    const formData = {
+      email: values.email || null,
+    };
+    fetch('https://education07.pythonanywhere.com/auth/users/reset_password/', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw response.json();
+        } else {
+          return response.json();
+        }
+      })
+      .then(async (res) => {
+        console.log('res', res);
+      })
+      .catch((Err) => {
+        Err.then((err) => err).then((err) => {
+          console.log('err', err);
+          message.warning(err?.detail);
+        });
+      });
   };
   return (
     <Wrapper>
-      {' '}
       <FormWrapper>
-        {' '}
-        <Form
-          name='normal_login'
-          className='login-form'
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name='email'
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Email!',
-              },
-            ]}
+        {!success ? (
+          <Form
+            name='normal_login'
+            className='login-form'
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
           >
-            <Input
-              prefix={<MailOutlined className='site-form-item-icon' />}
-              placeholder='Email'
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type='primary'
-              htmlType='submit'
-              className='login-form-button'
+            <Form.Item
+              name='email'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Email!',
+                },
+              ]}
             >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+              <Input
+                prefix={<MailOutlined className='site-form-item-icon' />}
+                placeholder='Email'
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='login-form-button'
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        ) : (
+          <div style={{ textAlign: 'center', width: '400px' }}>
+            <h2 style={{ color: 'green' }}>Your request has been accepted.</h2>
+            <p>Please, verify your email in your mail box!</p>
+          </div>
+        )}
       </FormWrapper>
     </Wrapper>
   );
